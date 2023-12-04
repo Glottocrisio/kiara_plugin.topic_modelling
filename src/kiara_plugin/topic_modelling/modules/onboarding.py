82b2b3e3365e187f7345a628eac827ad91606893
Mariella CC <MariellaCC@users.noreply.github.com>
2023-12-04 10:51:55 +0100

    feat: add onboarding from Zenodo module #1

--- src/kiara_plugin/topic_modelling/modules/onboarding.py
+++ src/kiara_plugin/topic_modelling/modules/onboarding.py
@@ -63,3 +63,63 @@ class CreateTableFromUrl(KiaraModule):
         except Exception as e:
             # Handle exceptions
             raise e
+
+class CreateTableFromZenodo(KiaraModule):
+    """
+    This module retrieves text files from a specified folder within a zip file hosted on Zenodo.
+    It takes the DOI and the name of the file as inputs.
+    It outputs a table with two columns: one for the file names and the other for the content of these files.
+
+    Dependencies:
+    - urllib.request: https://docs.python.org/3/library/urllib.request.html
+    - zipfile: https://docs.python.org/3/library/zipfile.html
+    - polars: https://www.pola.rs/
+    """
+
+    _module_type_name = "topic_modelling.create.table.from.zenodo"
+
+    def create_inputs_schema(self):
+        return {
+            "doi": {
+                "type": "string",
+                "doc": "The Digital Object Identifier for the resource."
+            },
+            "file_name": {
+                "type": "string",
+                "doc": "The name of the file to be processed."
+            }
+        }
+
+    def create_outputs_schema(self):
+        return {
+            "file_contents_table": {
+                "type": "table",
+                "doc": "A table with two columns: file names and their contents."
+            }
+        }
+
+    def process(self, inputs, outputs):
+        doi = inputs.get_value_data("doi")
+        file_name = inputs.get_value_data("file_name")
+        url = f"https://zenodo.org/record/{doi}/files/{file_name}"
+
+        # Download and extract the zip file
+        urllib.request.urlretrieve(url, "temp.zip")
+        with zipfile.ZipFile("temp.zip", 'r') as zip_ref:
+            zip_ref.extractall("temp_folder")
+
+        # Process text files and create the table
+        file_names = []
+        file_contents = []
+        for root, dirs, files in os.walk("temp_folder"):
+            for file in files:
+                if file.endswith(".txt"):
+                    file_path = os.path.join(root, file)
+                    with open(file_path, 'r') as f:
+                        content = f.read()
+                        file_names.append(file)
+                        file_contents.append(content)
+
+        # Create a table with polars
+        table = pl.DataFrame({"File Name": file_names, "Content": file_contents})
+        outputs.set_value("file_contents_table", table)

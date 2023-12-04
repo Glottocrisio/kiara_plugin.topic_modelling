630118bf359993e1893933f47f3320afa4cca56a
Mariella CC <MariellaCC@users.noreply.github.com>
2023-12-04 13:47:28 +0100

    fix: table data type in url module

--- src/kiara_plugin/topic_modelling/modules/onboarding.py
+++ src/kiara_plugin/topic_modelling/modules/onboarding.py
@@ -8,68 +8,66 @@ import os
 import polars as pl
 import pyarrow as pa
 
-# class CreateTableFromUrl(KiaraModule):
-#     """
-#     This module retrieves text files from a specified sub-path within a zip file hosted at a given URL.
-#     It outputs a table with two columns: one for the file names and the other for the content of these files.
+class CreateTableFromUrl(KiaraModule):
+    """
+    This module retrieves text files from a specified sub-path within a zip file hosted at a given URL.
+    It outputs a table with two columns: one for the file names and the other for the content of these files.
 
-#     Dependencies:
-#     - urllib.request: https://docs.python.org/3/library/urllib.request.html
-#     - zipfile: https://docs.python.org/3/library/zipfile.html
-#     - polars: https://www.pola.rs/
-#     """
+    Dependencies:
+    - urllib.request: https://docs.python.org/3/library/urllib.request.html
+    - zipfile: https://docs.python.org/3/library/zipfile.html
+    - polars: https://www.pola.rs/
+    """
 
-#     _module_type_name = "topic_modelling.create_table_from_url"
+    _module_type_name = "topic_modelling.create_table_from_url"
 
-#     def create_inputs_schema(self):
-#         return {
-#             "url": {
-#                 "type": "string",
-#                 "doc": "URL of the zip file to retrieve text files from."
-#             },
-#             "sub_path": {
-#                 "type": "string",
-#                 "doc": "Sub-path within the zip file to look for text files."
-#             }
-#         }
+    def create_inputs_schema(self):
+        return {
+            "url": {
+                "type": "string",
+                "doc": "URL of the zip file to retrieve text files from."
+            },
+            "sub_path": {
+                "type": "string",
+                "doc": "Sub-path within the zip file to look for text files."
+            }
+        }
 
-#     def create_outputs_schema(self):
-#         return {
-#             "corpus_table": {
-#                 "type": "table",
-#                 "doc": "Table with file names and contents."
-#             }
-#         }
+    def create_outputs_schema(self):
+        return {
+            "corpus_table": {
+                "type": "table",
+                "doc": "Table with file names and contents."
+            }
+        }
 
-#     def process(self, inputs, outputs):
+    def process(self, inputs, outputs):
         
-#         url = inputs.get_value_data("url")
-#         sub_path = inputs.get_value_data("sub_path")
+        url = inputs.get_value_data("url")
+        sub_path = inputs.get_value_data("sub_path")
 
-#         try:
-#             with urllib.request.urlopen(url) as response:
-#                 zip_file = zipfile.ZipFile(io.BytesIO(response.read()))
+        try:
+            with urllib.request.urlopen(url) as response:
+                zip_file = zipfile.ZipFile(io.BytesIO(response.read()))
 
-#             file_contents = []
+            file_contents = []
             
-#             for file in zip_file.namelist():
-#                 if file.startswith(sub_path) and file.endswith('.txt'):
-#                     with zip_file.open(file) as f:
-#                         content = f.read().decode('utf-8')
-#                     file_name = os.path.basename(file)
-#                     file_contents.append({'file_name': file_name, 'content': content})
-    
-#             pl_df = pl.DataFrame(file_contents)
-#             pa_table = pl_df.to_arrow()
+            for file in zip_file.namelist():
+                if file.startswith(sub_path) and file.endswith('.txt'):
+                    with zip_file.open(file) as f:
+                        content = f.read().decode('utf-8')
+                    file_name = os.path.basename(file)
+                    file_contents.append({'file_name': file_name, 'content': content})
             
-#             outputs['corpus_table'].set_value(pa_table)
+            pl_df = pl.DataFrame(file_contents)
+            pa_table = pl_df.to_arrow()
+            outputs.set_value("corpus_table", pa_table)
 
-#         except Exception as e:
-#             # Print exception details
-#             print(f"Error retrieving files: {e}")
+        except Exception as e:
+            # Print exception details
+            print(f"An error occurred: {e}")
 
         
-
 class CreateTableFromZenodo(KiaraModule):
     """
     This module retrieves text files from a specified folder hosted on Zenodo.

6110b011557469654fb1bb6c0dc1b48d332eff50
Mariella CC <MariellaCC@users.noreply.github.com>
2024-10-17 13:22:02 +0200

    feat: add corpus dist module

--- src/kiara_plugin/topic_modelling/modules/corpus_metadata.py
+++ src/kiara_plugin/topic_modelling/modules/corpus_metadata.py
@@ -2,6 +2,7 @@
 
 from kiara.api import KiaraModule
 from kiara.exceptions import KiaraProcessingException
+from typing import Union
 
 
 class GetLccnMetadata(KiaraModule):
@@ -119,4 +120,101 @@ class GetLccnMetadata(KiaraModule):
             raise KiaraProcessingException(e)
 
         outputs.set_value("corpus_table", output_table)
+
+
+class CorpusDistTime(KiaraModule):
+    """
+    This module aggregates a table by day, month or year from a corpus table that contains a date column. It returns the distribution over time, which can be used for display purposes, such as visualization.
+    
+    """
+
+    _module_type_name = "topic_modelling.corpus_distribution"
+
+    def create_inputs_schema(self):
+
+        return {
+            "periodicity": {
+                "type": "string",
+                "type_config": {"allowed_strings": ["day", "month", "year"]},
+                "doc": "The desired data periodicity to aggregate the data. Values can be either 'day','month' or 'year'.",
+                "optional": False,
+            },
+            "date_col": {
+                "type": "string",
+                "doc": "Column name of the column that contains the date. Values in this column need to comply with date format: https://docs.rs/chrono/latest/chrono/format/strftime/index.html.",
+                "optional": False,
+            },
+            "publication_ref_col": {
+                "type": "string",
+                "doc": "Column name of the values containing publication names or ref/id. This column will be used in the output.",
+                "optional": False,
+            },
+            "corpus_table": {
+                "type": "table",
+                "doc": "The corpus table for which the distribution over time is needed.",
+                "optional": False,
+            },
+        }
+
+    def create_outputs_schema(self):
+        return {"dist_table": {"type": "table", "doc": "The aggregated data table."}}
+
+    def process(self, inputs, outputs) -> None:
         
+        import duckdb # type: ignore
+        import polars as pl # type: ignore
+        import pyarrow as pa   # type: ignore
+
+        agg = inputs.get_value_obj("periodicity").data
+        title_col = inputs.get_value_obj("publication_ref_col").data
+        time_col = inputs.get_value_obj("date_col").data
+        table_obj = inputs.get_value_obj("corpus_table")
+
+
+        sources: Union[pa.Table, None] = table_obj.data
+            
+        sources_col_names = sources.column_names
+
+        if title_col not in sources_col_names:
+
+            raise KiaraProcessingException(
+                f"Could not find title name/id column '{title_col}' in the table. Please specify a valid column name manually, using one of: {', '.join(sources_col_names)}"
+            )
+            
+        if time_col not in sources_col_names:
+
+            raise KiaraProcessingException(
+                f"Could not find date column '{time_col}' in the table. Please specify a valid column name manually, using one of: {', '.join(sources_col_names)}"
+            )
+            
+        sources_data: pa.Table = table_obj.data.arrow_table
+            
+        sources_tb: pl.DataFrame = pl.from_arrow(sources_data) # type: ignore
+
+        try:
+            sources_tb = sources_tb.with_columns(
+                pl.col(time_col).str.strptime(pl.Date, "%Y-%m-%d")
+            )
+
+        except:
+            raise KiaraProcessingException(
+                f"Could not convert time column to a valid date format. Please check the pattern of source values."
+            )
+
+        if agg == "month":
+            query = f"SELECT EXTRACT(MONTH FROM date) AS month, EXTRACT(YEAR FROM date) AS year, {title_col}, COUNT(*) as count FROM sources_tb GROUP BY {title_col}, EXTRACT(YEAR FROM date), EXTRACT(MONTH FROM date)"  # noqa
+        elif agg == "year":
+            query = f"SELECT EXTRACT(YEAR FROM date) AS year, {title_col}, COUNT(*) as count FROM sources_tb GROUP BY {title_col}, EXTRACT(YEAR FROM date)"  # noqa
+        elif agg == "day":
+            query = f"SELECT date, {title_col}, COUNT(*) as count FROM sources_tb GROUP BY {title_col}, date"  # noqa
+        
+        else:
+            raise KiaraProcessingException(
+                f'Please check the value chosen for periodicity. Supported values are "day", "month" or "year"'
+            )
+
+        queried_df = duckdb.query(query)
+
+        pa_out_table = queried_df.arrow()
+
+        outputs.set_value("dist_table", pa_out_table)
